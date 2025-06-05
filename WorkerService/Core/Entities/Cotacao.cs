3aca652b1223bdddb8595846896750253ef28421
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-06-04 22:30:32 -0300

    feat: entities

---
+++ WorkerService/Core/Entities/Cotacao.cs
@@ -0,0 +1,19 @@
+﻿namespace Core.Entities;
+
+public sealed class Cotacao
+{
+    public int IdCotacao { get; private set; }
+    public int FkIdAtivo { get; private set; }
+    public decimal PrecoUnitario { get; private set; }
+    public DateTime DataHora { get; private set; }
+    public Ativo? Ativo { get; private set; }
+
+    private Cotacao() { }
+
+    public Cotacao(int fkIdAtivo, decimal precoUnitario, DateTime dataHora)
+    {
+        FkIdAtivo = fkIdAtivo;
+        PrecoUnitario = precoUnitario;
+        DataHora = dataHora;
+    }
+}
