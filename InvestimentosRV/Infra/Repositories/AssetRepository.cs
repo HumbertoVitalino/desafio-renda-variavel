66e179036aedc4c11b53420590df419df31f861a
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-06-06 16:41:31 -0300

    feat: repositories

--- InvestimentosRV/Infra/Repositories/AssetRepository.cs
+++ InvestimentosRV/Infra/Repositories/AssetRepository.cs
@@ -1,8 +1,15 @@
 ﻿using Core.Domain;
 using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infra.Repositories;
 
 public class AssetRepository(AppDbContext context) : Repository<Asset>(context), IAssetRepository
 {
+    public async Task<Asset?> GetByTickerAsync(string tickerSymbol, CancellationToken cancellationToken)
+    {
+        return await _context.Set<Asset>()
+            .AsNoTracking()
+            .FirstOrDefaultAsync(a => a.TickerSymbol.Equals(tickerSymbol), cancellationToken);
+    }
 }
