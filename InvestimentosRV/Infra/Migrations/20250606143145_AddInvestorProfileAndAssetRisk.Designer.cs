ac5c1111a3f63a3ae7f3388e7eae78982006490e
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-06-06 11:42:21 -0300

    feat: migrations

--- InvestimentosRV/Infra/Migrations/20250606143145_AddInvestorProfileAndAssetRisk.Designer.cs
+++ InvestimentosRV/Infra/Migrations/20250606143145_AddInvestorProfileAndAssetRisk.Designer.cs
@@ -4,6 +4,7 @@ using Infra.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infra.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250606143145_AddInvestorProfileAndAssetRisk")]
+    partial class AddInvestorProfileAndAssetRisk
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
