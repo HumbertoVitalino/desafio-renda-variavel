7fae1ac74219d6e6c70daa8cfc9eac70fea2b040
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-06-06 11:41:04 -0300

    feat: login user boundaries

---
+++ InvestimentosRV/Core/UseCase/LoginUserUseCase/Boundaries/LoginUserInput.cs
@@ -0,0 +1,9 @@
+﻿using Core.Commons;
+using MediatR;
+
+namespace Core.UseCase.LoginUserUseCase.Boundaries;
+
+public sealed record LoginUserInput(
+    string Email,
+    string Password
+) : IRequest<Output>;
---
+++ InvestimentosRV/Core/UseCase/LoginUserUseCase/Boundaries/LoginUserInputValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace Core.UseCase.LoginUserUseCase.Boundaries;
+
+public class LoginUserInputValidator : AbstractValidator<LoginUserInput>
+{
+	public LoginUserInputValidator()
+	{
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("The email is required.")
+            .EmailAddress().WithMessage("The email format is invalid.");
+
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("The password is required.")
+            .MinimumLength(8).WithMessage("The password must be at least 8 characters long.")
+            .Matches("[A-Z]").WithMessage("The password must contain at least one uppercase letter.")
+            .Matches("[a-z]").WithMessage("The password must contain at least one lowercase letter.")
+            .Matches("[0-9]").WithMessage("The password must contain at least one number.");
+    }
+}
