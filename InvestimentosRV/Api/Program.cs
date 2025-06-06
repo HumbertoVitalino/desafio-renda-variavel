e759a26173456a9ebf14903af6b36db56cc8b3ad
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-06-05 23:39:28 -0300

    feat: api start

---
+++ InvestimentosRV/Api/Program.cs
@@ -0,0 +1,29 @@
+using Core.IoC;
+using Infra.IoC;
+
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddJwt(configuration);
+builder.Services.AddMediatr(configuration);
+builder.Services.AddInfra(configuration);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
