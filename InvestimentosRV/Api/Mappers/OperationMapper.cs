4eed852696f35ee2f15cb4b9bde8e4c9273e2c6e
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-06-06 16:42:14 -0300

    feat: mappers

---
+++ InvestimentosRV/Api/Mappers/OperationMapper.cs
@@ -0,0 +1,17 @@
+﻿using Api.Requests;
+using Core.UseCase.NewOperationUseCase.Boundaries;
+
+namespace Api.Mappers;
+
+public static class OperationMapper
+{
+    public static NewOperationInput MapToInput(this NewOperationRequest request, int userId)
+    {
+        return new NewOperationInput(
+            userId,
+            request.TickerSymbol,
+            request.Quantity,
+            request.Type
+        );
+    }
+}
---
+++ InvestimentosRV/Core/Mappers/PositionMapper.cs
@@ -0,0 +1,17 @@
+﻿using Core.Domain;
+using Core.UseCase.NewOperationUseCase.Boundaries;
+
+namespace Core.Mappers;
+
+public static class PositionMapper
+{
+    public static Position MapPositionToDomain(this NewOperationInput input, int assetId, decimal executionPrice)
+    {
+        return new Position(
+            input.UserId,
+            assetId,
+            input.Quantity,
+            executionPrice
+        );
+    }
+}
