0a79b238603a187e719dd10acb4304d37356042c
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-06-05 23:41:01 -0300

    feat: di

---
+++ InvestimentosRV/Infra/IoC/DependencyInjection.cs
@@ -0,0 +1,32 @@
+﻿using Core.Interfaces;
+using Infra.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infra.IoC;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfra(this IServiceCollection services, IConfiguration configuration)
+    {
+        services
+            .AddRepositories(configuration);
+        return services;
+    }
+
+    public static IServiceCollection AddRepositories(this IServiceCollection serviceCollection, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+        serviceCollection.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+        });
+
+        serviceCollection.AddScoped<IUnitOfWork, UnitOfWork>();
+        serviceCollection.AddScoped<IUserRepository, UserRepository>();
+        serviceCollection.AddScoped<IPasswordService, PasswordService>();
+
+        return serviceCollection;
+    }
+}
