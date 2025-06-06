ca65eac7c98940c090bc6d69eb1324cf0c855cfb
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-06-05 23:40:43 -0300

    feat: interfaces

---
+++ InvestimentosRV/Core/Interfaces/IPasswordService.cs
@@ -0,0 +1,7 @@
+﻿namespace Core.Interfaces;
+
+public interface IPasswordService
+{
+    (byte[] passwordHash, byte[] passwordSalt) CreatePasswordHash(string password);
+    bool VerifyPasswordHash(string password, byte[] storedHash, byte[] storedSalt);
+}
---
+++ InvestimentosRV/Core/Interfaces/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using Core.Domain;
+
+namespace Core.Interfaces;
+
+public interface IRepository<T> where T : Entity
+{
+    Task CreateAsync(T entity, CancellationToken cancellationToken);
+    void Update(T entity);
+    void Delete(T entity);
+    Task<T?> GetAsync(int id, CancellationToken cancellationToken);
+    Task<IEnumerable<T>> GetAllAsync(CancellationToken cancellationToken);
+}
