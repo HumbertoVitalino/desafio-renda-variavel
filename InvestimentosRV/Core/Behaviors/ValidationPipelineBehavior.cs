a3fdba30fec4f77f4f53f28b1d673c6fdaf9832c
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-06-06 11:42:09 -0300

    feat: behavior

---
+++ InvestimentosRV/Core/Behaviors/ValidationPipelineBehavior.cs
@@ -0,0 +1,45 @@
+﻿using Core.Commons;
+using FluentValidation;
+using MediatR;
+
+namespace Core.Behaviors;
+
+public class ValidationPipelineBehavior<TRequest, TResponse>(IEnumerable<IValidator<TRequest>> validators) : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+    where TResponse : Output, new()
+{
+    private readonly IEnumerable<IValidator<TRequest>> _validators = validators;
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        if (!_validators.Any())
+        {
+            return await next(cancellationToken);
+        }
+
+        var context = new ValidationContext<TRequest>(request);
+
+        var validationResults = await Task.WhenAll(
+            _validators.Select(v => v.ValidateAsync(context, cancellationToken))
+        );
+
+        var failures = validationResults
+            .SelectMany(r => r.Errors)
+            .Where(f => f != null)
+            .ToList();
+
+        if (failures.Count != 0)
+        {
+            var response = new TResponse();
+
+            foreach (var failure in failures)
+            {
+                response.AddErrorMessage(failure.ErrorMessage);
+            }
+
+            return response;
+        }
+
+        return await next(cancellationToken);
+    }
+}
