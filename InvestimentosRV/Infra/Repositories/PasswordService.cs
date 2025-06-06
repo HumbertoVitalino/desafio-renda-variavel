8f074fe7c7fd93c1720048fc00e4713ab82ebad3
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-06-05 23:40:56 -0300

    feat: repositories + service

---
+++ InvestimentosRV/Infra/Repositories/PasswordService.cs
@@ -0,0 +1,39 @@
+﻿using Core.Interfaces;
+using System.Security.Cryptography;
+
+namespace Infra.Repositories;
+
+public class PasswordService : IPasswordService
+{
+    private const int Iterations = 10000;
+    private const int SaltSize = 16;
+    private const int HashSize = 32;
+
+    public (byte[] passwordHash, byte[] passwordSalt) CreatePasswordHash(string password)
+    {
+        var salt = RandomNumberGenerator.GetBytes(SaltSize);
+
+        var hash = Rfc2898DeriveBytes.Pbkdf2(
+            password,
+            salt,
+            Iterations,
+            HashAlgorithmName.SHA256,
+            HashSize
+        );
+
+        return (hash, salt);
+    }
+
+    public bool VerifyPasswordHash(string password, byte[] storedHash, byte[] storedSalt)
+    {
+        var hashToCompare = Rfc2898DeriveBytes.Pbkdf2(
+            password,
+            storedSalt,
+            Iterations,
+            HashAlgorithmName.SHA256,
+            HashSize
+        );
+
+        return CryptographicOperations.FixedTimeEquals(hashToCompare, storedHash);
+    }
+}
---
+++ InvestimentosRV/Infra/Repositories/Repository.cs
@@ -0,0 +1,37 @@
+﻿using Core.Domain;
+using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Repositories;
+
+public class Repository<T>(AppDbContext context) : IRepository<T> where T : Entity
+{
+    protected readonly AppDbContext _context = context;
+
+    public async Task CreateAsync(T entity, CancellationToken cancellationToken) =>
+        await _context.Set<T>().AddAsync(entity, cancellationToken);
+
+    public void Delete(T entity)
+    {
+        _context.Remove(entity);
+    }
+
+    public async Task<IEnumerable<T>> GetAllAsync(CancellationToken cancellationToken)
+    {
+        return await _context.Set<T>()
+            .AsNoTracking()
+            .ToListAsync(cancellationToken);
+    }
+
+    public async Task<T?> GetAsync(int id, CancellationToken cancellationToken)
+    {
+        return await _context.Set<T>()
+            .FindAsync([id], cancellationToken);
+    }
+
+    public void Update(T entity)
+    {
+        entity.UpdatedAt = DateTime.UtcNow;
+        _context.Update(entity);
+    }
+}
