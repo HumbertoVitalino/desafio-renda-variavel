88c0baeba889d54102c1f79fabe6133f83f49553
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-06-07 00:56:46 -0300

    feat: api/quote

---
+++ InvestimentosRV/Api/Controller/QuotesController.cs
@@ -0,0 +1,34 @@
+﻿using Api.Requests;
+using Core.Commons;
+using Core.UseCase.GetLatestQuoteUseCase.Boundaries;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controller;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+[Authorize]
+public class QuotesController(
+    IMediator mediator    
+) : BaseController
+{
+    private readonly IMediator _mediator = mediator;
+
+    [HttpGet("{tickerSymbol}/latest")]
+    [ProducesResponseType(typeof(Output), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(Output), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> GetLatestQuoteAsync([FromRoute] string tickerSymbol, CancellationToken cancellationToken)
+    {
+        GetLatestQuoteInput input = new(tickerSymbol); 
+
+        var output = await _mediator.Send(input, cancellationToken);
+
+        if (!output.IsValid)
+            return NotFound(output);
+
+        return Ok(output);
+    }
+}
