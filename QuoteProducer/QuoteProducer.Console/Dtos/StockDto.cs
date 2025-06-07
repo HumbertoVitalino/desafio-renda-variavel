20ee7a9f965eac1c9e740a20a36621a1c242f24f
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-06-07 18:24:52 -0300

    feat: console app producer

---
+++ QuoteProducer/QuoteProducer.Console/Dtos/StockDto.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json.Serialization;
+
+namespace QuoteProducer.Console.Dtos;
+
+public sealed record StockDto
+{
+   [JsonPropertyName("ticker")]
+   public string Ticker { get; init; } = string.Empty;
+
+   [JsonPropertyName("price")]
+   public decimal Price { get; init; }
+
+   [JsonPropertyName("name")]
+   public string Name { get; init; } = string.Empty;
+
+   [JsonPropertyName("change")]
+   public decimal Change { get; init; }
+
+   [JsonPropertyName("closeyest")]
+   public decimal YesterdayClosePrice { get; init; }
+
+   [JsonPropertyName("volume")]
+   public long Volume { get; init; }
+
+   [JsonPropertyName("marketcap")]
+   public long? MarketCap { get; init; }
+
+   [JsonPropertyName("tradetime")]
+   public DateTime TradeTime { get; init; }
+}
